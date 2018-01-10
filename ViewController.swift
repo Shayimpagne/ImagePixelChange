b81a12792710286f29f9d08b00e35816b5ae9783
Emir Shayymov <emirxl95@mail.ru>
2018-01-10 17:01:39 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  ImagePixelChange
+//
+//  Created by Emir on 1/9/18.
+//  Copyright © 2018 Emir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var original:UIImageView!
+    @IBOutlet var result:UIImageView!
+    var array:[UIColor] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        original.image = UIImage(named: "4")
+        
+        result.image = processPixels(in: UIImage(named: "4")!)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func processPixels(in image: UIImage) -> UIImage? {
+        guard let inputCGImage = image.cgImage else {
+            print("unable to get cgImage")
+            return nil
+        }
+        let colorSpace       = CGColorSpaceCreateDeviceRGB()
+        let width            = inputCGImage.width
+        let height           = inputCGImage.height
+        let bytesPerPixel    = 4
+        let bitsPerComponent = 8
+        let bytesPerRow      = bytesPerPixel * width
+        let bitmapInfo       = RGBA32.bitmapInfo
+        
+        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else {
+            print("unable to create context")
+            return nil
+        }
+        context.draw(inputCGImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        guard let buffer = context.data else {
+            print("unable to get context data")
+            return nil
+        }
+        
+        var pixelBuffer = buffer.bindMemory(to: RGBA32.self, capacity: width * height)
+        
+        for row in 0 ..< Int(height) {
+            for column in 0 ..< Int(width) {
+                let offset = row * width + column
+                for i in 10...255{
+                    var mygreen = RGBA32(red: 0,   green: UInt8(i), blue: 0,   alpha: 255)
+                    if pixelBuffer[offset] == mygreen{
+                        pixelBuffer[offset] = .red
+                    }
+                }
+                    
+            }
+        }
+        
+        let outputCGImage = context.makeImage()!
+        let outputImage = UIImage(cgImage: outputCGImage, scale: image.scale, orientation: image.imageOrientation)
+        
+        return outputImage
+    }
+    
+    struct RGBA32: Equatable {
+        private var color: UInt32
+        
+        var redComponent: UInt8 {
+            return UInt8((color >> 24) & 255)
+        }
+        
+        var greenComponent: UInt8 {
+            return UInt8((color >> 16) & 255)
+        }
+        
+        var blueComponent: UInt8 {
+            return UInt8((color >> 8) & 255)
+        }
+        
+        var alphaComponent: UInt8 {
+            return UInt8((color >> 0) & 255)
+        }
+        
+        init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8) {
+            color = (UInt32(red) << 24) | (UInt32(green) << 16) | (UInt32(blue) << 8) | (UInt32(alpha) << 0)
+        }
+        
+        static let red     = RGBA32(red: 255, green: 0,   blue: 0,   alpha: 255)
+        static let green   = RGBA32(red: 0,   green: 255, blue: 0,   alpha: 255)
+        
+        static let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+        
+        static func ==(lhs: RGBA32, rhs: RGBA32) -> Bool {
+            return lhs.color == rhs.color
+        }
+    }
+}
+
+
+
+
